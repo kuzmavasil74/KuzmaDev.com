71ca43214446f8a09b5fd9c5fc77fa336d916d17
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2024-09-22 21:52:41 +0200

    add_header_1

--- kuzma_dev_com/src/App.jsx
+++ kuzma_dev_com/src/App.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
-import { Route, Router, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import Header from './components/Header/Header.jsx'
 import Footer from './components/Footer/Footer.jsx'
+import About from './pages/About/About.jsx'
+import Projects from './pages/Projects/Projects.jsx'
+import Skills from './pages/Skills/Skills.jsx'
+import Contact from './pages/Contact/Contact.jsx'
+import Blog from './pages/Blog/Blog.jsx'
+import Resume from './pages/Resume/Resume.jsx'
 
-import styles from './App.css'
+import styles from './App.module.css'
 
 function App() {
   return (
-    <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/projects" element={<Projects />}></Route>
-          <Route path="/skills" element={<Skills />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
-          <Route path="/blog" element={<Blog />}></Route>
-          <Route path="/resume" element={<Resume />}></Route>
-          <Route path="*" element={<Home />}></Route>
-        </Routes>
-        <Footer />
-      </Router>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/skills" element={<Skills />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/blog" element={<Blog />}></Route>
+        <Route path="/resume" element={<Resume />}></Route>
+        <Route path="*" element={<Home />}></Route>
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
--- kuzma_dev_com/src/components/Header/Header.jsx
+++ kuzma_dev_com/src/components/Header/Header.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import styles from './Header.module.css'
 function Header() {
-  return <div>Header</div>
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerLogo}>
+        <Link to="/" className={styles.headerLogoLink}>
+          <img
+            src="../../../public/images/KuzmaDevComLogo.svg"
+            alt="KuzmaDev Logo"
+            width="80px"
+            className={styles.headerLogoImage}
+          />
+        </Link>
+      </div>
+      <div className={styles.headerChangeLanguage}></div>
+      <nav className={styles.headerNav}>
+        <ul className={styles.headerNavList}>
+          <li>
+            <Link to="/about" className={styles.headerNavListItem}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/projects" className={styles.headerNavListItem}>
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link to="/skills" className={styles.headerNavListItem}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className={styles.headerNavListItem}>
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link to="/blog" className={styles.headerNavListItem}>
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link to="/resume" className={styles.headerNavListItem}>
+              Resume
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
