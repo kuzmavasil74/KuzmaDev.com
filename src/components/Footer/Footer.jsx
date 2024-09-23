ad902a967e8823571f184b941436de8383d1720e
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2024-09-23 22:07:22 +0200

    add_footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,102 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import styles from './Footer.module.css'
 function Footer() {
-  return <div>Footer</div>
+  return (
+    <footer className={styles.footerContainer}>
+      <div className={styles.footerLogo}>
+        <img
+          src="/images/KuzmaDevComLogo.svg"
+          alt="KuzmaDev Logo"
+          width="80px"
+          className={styles.footerLogoImage}
+        />
+      </div>
+      <nav className={styles.footerQuickLinks}>
+        <ul className={styles.footerQuickLinksList}>
+          <li>
+            <Link to="/about" className={styles.footerQuickLinksListItem}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/projects" className={styles.footerQuickLinksListItem}>
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link to="/skills" className={styles.footerQuickLinksListItem}>
+              Skills
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <nav className={styles.footerSocialLinks}>
+        <ul className={styles.footerSocialLinksList}>
+          <li>
+            <a
+              href="https://github.com/kuzmavasil74"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.footerSocialLinksListItem}
+            >
+              <img
+                className={styles.footerSocialLinksListItemImage}
+                src="/images/gitHubLogo.svg"
+                alt="github-link"
+                width="20px"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/kuzmavasil"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.footerSocialLinksListItem}
+            >
+              <img
+                className={styles.footerSocialLinksListItemImage}
+                src="/images/instagramLogo.svg"
+                alt="instagram-link"
+                width="20px"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="tel:+420 776 788 319"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.footerSocialLinksListItem}
+            >
+              <img
+                className={styles.footerSocialLinksListItemImage}
+                src="/images/telephoneLogo.svg"
+                alt="telephone-link"
+                width="20px"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:kuzmavasil.v@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.footerSocialLinksListItem}
+            >
+              <img
+                className={styles.footerSocialLinksListItemImage}
+                src="/images/mailLogo.svg"
+                alt="mail-link"
+                width="20px"
+              />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  )
 }
 export default Footer
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import styles from './Home.module.css'
 function Home() {
-  return <div>Home</div>
+  return <div className={styles.home}></div>
 }
 
 export default Home
