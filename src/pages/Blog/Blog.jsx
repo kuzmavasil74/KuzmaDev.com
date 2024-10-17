0d38e64d90f2be872390a8bc2e8ab3f21fa19fee
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2024-10-18 00:09:46 +0200

    start_page_blogs

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
 
 import styles from './Blog.module.css'
 
@@ -7,6 +8,31 @@ function Blog() {
   return (
     <div>
       <Header />
+      <section className={styles.blogSection}>
+        <div className={styles.blogHeader}>
+          <h2 className={styles.blogSectionTitle}>Blogs</h2>
+          <p className={styles.blogSectionTitleDesc}>
+            My latest{' '}
+            <span className={styles.blogSectionListItemDescSpan}>
+              blog posts.
+            </span>
+          </p>
+        </div>
+        <ul className={styles.blogSectionList}>
+          <li className={styles.blogSectionListItem}>
+            <a href="/images/blogs/post-one.jpg" alt="post-one">
+              <div className={styles.blogSectionListItemImgCard}>
+                <img className={styles.blogSectionListItemImg} src="" alt="" />
+                <div className={styles.blogSectionListItemText}>
+                  <h3 className={styles.blogSectionListItemTextTitle}></h3>
+                  <p className={styles.blogSectionListItemTextDesc}></p>
+                </div>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </section>
+      <Footer />
     </div>
   )
 }
